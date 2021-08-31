6900cfa0b0522e1729412eaae7e61dd90933816e
Andrei Brovko <andrei.brovko@ihsmarkit.com>
2021-08-31 15:30:16 +0300

    init project add gitignore file

---
+++ WireMock Trainee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WireMock_Trainee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
